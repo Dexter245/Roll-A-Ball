3fb6a77718cffc3e5d12ebc7b98300ce16c04fff
Lajos Ambrus <Dexter245@googlemail.com>
2019-08-08 16:41:15 +0200

    right analog stick now moves the camera around the player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    const float MAX_ROTATION = 30f;
     public GameObject player;
 
     private Vector3 offset;
+    private Quaternion initRotation;
 
     // Start is called before the first frame update
     void Start()
     {
         offset = transform.position - player.transform.position;
+        initRotation = transform.rotation;
     }
 
     // Update is called once per frame
@@ -23,5 +26,14 @@ public class CameraController : MonoBehaviour
     private void LateUpdate()
     {
         transform.position = player.transform.position + offset;
+        float rotateHorizontal = Input.GetAxis("Horizontal2");
+        float rotateVertical = Input.GetAxis("Vertical2");
+        Vector3 rotation = new Vector3(-rotateVertical * MAX_ROTATION, rotateHorizontal * MAX_ROTATION, 0f);
+        //Debug.Log("h2: " + rotateHorizontal + ", v2: " + rotateVertical);
+        Debug.Log("rotation: " + rotation);
+        transform.rotation = initRotation;
+        //transform.Rotate(rotation, Space.Self);
+        transform.RotateAround(player.transform.position, Vector3.up, rotateHorizontal * MAX_ROTATION);
+        transform.RotateAround(player.transform.position, Vector3.right, rotateVertical * MAX_ROTATION);
     }
 }
