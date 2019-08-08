45ff80dbd5348217285161ad74383e6a9ffae09d
Lajos Ambrus <Dexter245@googlemail.com>
2019-08-08 13:54:03 +0200

    First commit of finished tutorial project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Text countText;
+    public Text winText;
+    public float speed = 1f;
+
+    private Rigidbody rb;
+    private int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        SetCountText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // FixedUpdate is called before any physics calc is done
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter");
+        if (other.CompareTag("Pickup"))
+        {
+            Debug.Log("blob");
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Collected: " + count;
+        if(count >= 8)
+        {
+            winText.text = "You're Winner!";
+        }
+    }
+}
